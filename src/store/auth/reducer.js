6801b2684672945745d869be2cec1b2ccdb75440
Diego Haz <hazdiego@gmail.com>
2016-11-27 05:06:24 -0200

    Fix auth reducer indentation

--- src/store/auth/reducer.js
+++ src/store/auth/reducer.js
@@ -3,12 +3,12 @@ import { AUTH_FACEBOOK_SUCCESS } from './actions'
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case AUTH_FACEBOOK_SUCCESS:
-      return {
-        ...state,
-        token: action.token
-      }
-    default:
-      return state
+  case AUTH_FACEBOOK_SUCCESS:
+    return {
+      ...state,
+      token: action.token
+    }
+  default:
+    return state
   }
 }
