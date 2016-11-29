9314f3812203e622f23f046aeb735d168dc9a602
Diego Haz <hazdiego@gmail.com>
2016-11-29 20:36:54 -0200

    Add primary colors

--- src/components/globals.js
+++ src/components/globals.js
@@ -1,7 +1,7 @@
 import { keyframes } from 'styled-components'
 
 export const colors = {
-  primary: ['#1976d2', '#2196f3', '#71bcf7', '#c2e2fb'],
+  primary: ['#d06e04', '#fe8604', '#feb25f', '#fec88c', '#fedeba'],
   secondary: ['#c2185b', '#e91e63', '#f06292', '#f8bbd0'],
   danger: ['#d32f2f', '#f44336', '#f8877f', '#ffcdd2'],
   alert: ['#ffa000', '#ffc107', '#ffd761', '#ffecb3'],
