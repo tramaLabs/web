9c6db08a8561a3eefa3a92399cb3fc57210a625d
Diego Haz <hazdiego@gmail.com>
2016-12-02 00:17:10 -0200

    Add LogoLink component

---
+++ src/components/molecules/LogoLink/index.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import LogoLink from '.'
+
+const wrap = (props = {}) => shallow(<LogoLink {...props} />)
+
+it('renders props when passed in', () => {
+  const wrapper = wrap({ id: 'foo' })
+  expect(wrapper.find({ id: 'foo' }).length).toBeGreaterThan(0)
+})
--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,3 +1,4 @@
 export Field from './Field'
 export IconButton from './IconButton'
+export LogoLink from './LogoLink'
 export TagLink from './TagLink'
