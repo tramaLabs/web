f280833f809ae6c37e39186092328300385f781d
Diego Haz <hazdiego@gmail.com>
2016-12-03 03:57:25 -0200

    Update HomeHero component

--- src/components/organisms/HomeHero/index.js
+++ src/components/organisms/HomeHero/index.js
@@ -1,67 +1,82 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Grid, Row } from 'react-bootstrap'
 
 import { colors } from 'components/globals'
-import { Heading, Button } from 'components'
-import { SearchForm } from 'containers'
+import { Paragraph, Heading, Button } from 'components'
+import SearchForm from 'containers/SearchForm'
 import background from './background.jpg'
 
 const Wrapper = styled.div`
-  display: table;
-  width: 100%;
-  padding: 0;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  height: 100vh;
+  min-height: 500px;
+  max-height: 700px;
+  padding: 2rem 6rem;
+  box-sizing: border-box;
   background: url(${background}) no-repeat center center;
-  background-color: #e5e5e5;
   background-size: cover;
-`
-const Overlay = styled.div`
-  background: rgba(0,0,0,0.4);
-`
-const HeroText = styled.div`
-  padding-top: 150px;
-  padding-bottom: 50px;
+  background-color: white;
   text-align: center;
-`
-const Herop = styled.div`
-  color:white;
-  font-size:27px;
-  font-weight: 300;
-  padding: 0px 170px;
-`
-const HeroCall = styled.div`
-  text-align: center;
-  padding-top: 20px;
-  padding-bottom: 20px;
-  background-color: rgba(240,130,31,0.4);
+  @media screen and (max-width: 640px) {
+    padding: 1rem;
+  }
 `
 
-const HomeHero = ({ ...props }) => {
+const Shadow = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: radial-gradient(closest-corner at 50% 25%, transparent -100%, black 300%);
+`
+
+const Main = styled.div`
+  position: relative;
+`
+
+const StyledParagraph = styled(Paragraph)`
+  color: ${[ ...colors.grayscale ].reverse()[0]};
+  margin: 3rem auto;
+  max-width: 800px;
+  font-size: 1.625rem;
+  line-height: 1.325em;
+  letter-spacing: 0.07em;
+  @media screen and (max-width: 640px) {
+    font-size: 1rem;
+  }
+`
+
+const SearchBox = styled.div`
+  position: relative;
+  margin: 0.5rem;
+  width: 100%;
+  max-width: 600px;
+`
+
+const StyledHeading = styled(Heading)`
+  color: white;
+`
+
+const HomeHero = (props) => {
   return (
-    <header {...props} >
-      <Wrapper>
-        <Overlay>
-          <Grid>
-            <Row>
-              <HeroText>
-                <Herop>
-                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ligula felis, fringilla sit amet porta et, suscipit eget risus. Mauris vestibulum eros tempor, lobortis magna nec, fringilla massa.
-                          </Herop>
-                <Button to="#about" kind="grayscale" transparent light size={50}>
-                              Crie sua iniciativa
-                            </Button>
-              </HeroText>
-            </Row>
-          </Grid>
-          <HeroCall>
-            <Heading level={2} style={{fontSize: 27, fontWeight: 'bold', color: colors.grayscale[4]}}>
-                        Colabore com iniciativas
-                    </Heading>
-            <SearchForm kind="grayscale" transparent light borderless />
-          </HeroCall>
-        </Overlay>
-      </Wrapper>
-    </header>
+    <Wrapper {...props}>
+      <Shadow />
+      <Main>
+        <StyledParagraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ligula felis, fringilla sit amet porta et, suscipit eget risus. Mauris vestibulum eros tempor, lobortis magna nec, fringilla massa.
+        </StyledParagraph>
+        <Button to="/iniciativas/criar" size={54}>Criar iniciativa</Button>
+      </Main>
+      <SearchBox>
+        <StyledHeading level={2}>Colabore com iniciativas</StyledHeading>
+        <SearchForm kind="grayscale" transparent light borderless />
+      </SearchBox>
+    </Wrapper>
   )
 }
 
