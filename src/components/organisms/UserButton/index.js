231deb190ae41942afbb53e4204117c51b8923a1
Diego Haz <hazdiego@gmail.com>
2016-12-11 00:36:38 -0200

    Update UserButton styles

--- src/components/organisms/UserButton/index.js
+++ src/components/organisms/UserButton/index.js
@@ -4,17 +4,11 @@ import styled from 'styled-components'
 import { Dropdown, IconButton, Menu, MenuItem } from 'components'
 import FacebookLoginButton from 'containers/FacebookLoginButton'
 
-const StyledIconButton = styled(IconButton)`
-  @media screen and (min-width: 421px) {
-    padding: 0 0.5rem;
-  }
-`
-
 const ButtonWrapper = styled.div`
   display: flex;
   align-items: center;
   & img {
-    margin-right: 0.5rem;
+    margin: 0 0.666em 0 -0.666em;
   }
 `
 
@@ -22,12 +16,12 @@ const UserButton = ({ user, onUserLogout, ...props }) => {
   if (user) {
     return (
       <Dropdown onSelection={(value) => value()}>
-        <StyledIconButton type="menu" icon="down" right {...props}>
+        <IconButton type="menu" icon="down" right {...props}>
           <ButtonWrapper>
             <img src={user.picture} alt={user.name} width={24} height={24} />
             <span>{user.name}</span>
           </ButtonWrapper>
-        </StyledIconButton>
+        </IconButton>
         <Menu right>
           <MenuItem value={onUserLogout}>Sair</MenuItem>
         </Menu>
