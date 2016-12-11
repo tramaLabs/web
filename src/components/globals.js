02a810fdb65cd47938ca841241d9951c42bbc2dc
Diego Haz <hazdiego@gmail.com>
2016-12-11 00:14:46 -0200

    Update component globals

--- src/components/globals.js
+++ src/components/globals.js
@@ -6,8 +6,17 @@ export const colors = {
   danger: ['#d32f2f', '#f44336', '#f8877f', '#ffcdd2'],
   alert: ['#ffa000', '#ffc107', '#ffd761', '#ffecb3'],
   success: ['#388e3c', '#4caf50', '#7cc47f', '#c8e6c9'],
-  grayscale: ['#212121', '#616161', '#9e9e9e', '#bdbdbd', '#e0e0e0', '#eeeeee', '#ffffff']
+  grayscale: ['#212121', '#616161', '#9e9e9e', '#bdbdbd', '#e0e0e0', '#eeeeee', '#ffffff'],
+  alpha: [0.9, 0.6, 0.3, 0.1, 0].map((alpha) => `rgba(0, 0, 0, ${alpha})`)
 }
+
+export const reverseColors = {}
+
+Object.keys(colors).forEach((key) => {
+  reverseColors[key] = [ ...colors[key] ].reverse()
+})
+
+reverseColors.alpha = [1, 0.8, 0.6, 0.4, 0.2, 0].map((alpha) => `rgba(255, 255, 255, ${alpha})`)
 
 export const fonts = {
   primary: 'Lato, Helvetica Neue, Helvetica, Roboto, sans-serif',
@@ -17,11 +26,17 @@ export const fonts = {
 
 export const animations = {
   rotate360: keyframes`
-    from {
-      transform: rotate(0deg);
-    }
-    to {
-      transform: rotate(360deg);
-    }
+    from { transform: rotate(0deg); }
+    to { transform: rotate(360deg); }
+  `,
+  fadeIn: keyframes`
+    0% { display: none; opacity: 0; }
+    1% { display: block: opacity: 0; }
+    100% { display: block; opacity: 1; }
+  `,
+  fadeOut: keyframes`
+    0% { display: block; opacity: 1; }
+    1% { display: none: opacity: 1; }
+    100% { display: none; opacity: 0; }
   `
 }
