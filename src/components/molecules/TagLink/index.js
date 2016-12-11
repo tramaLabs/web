5a3c354d9eb66ec4a58f11bf99581236fc9b68b8
Diego Haz <hazdiego@gmail.com>
2016-12-11 00:33:16 -0200

    Add TagLink component

--- src/components/molecules/TagLink/index.js
+++ src/components/molecules/TagLink/index.js
@@ -2,26 +2,34 @@ import React, { PropTypes } from 'react'
 import styled from 'styled-components'
 
 import { colors, fonts } from 'components/globals'
+import { Link } from 'components'
 
-const Div = styled.div`
-  display: block;
+const StyledLink = styled(Link)`
+  display: inline-flex;
+  align-items: center;
   font-family: ${fonts.primary};
-  color: ${colors.grayscale[1]};
-  font-weight: 300;
-  font-style: normal;
-  margin-top: 0.4rem;
+  font-size: 0.875rem;
+`
+
+const Square = styled.span`
+  display: block;
+  width: 0.75em;
+  height: 0.75em;
+  margin-right: 0.5em;
+  background-color: ${colors.primary[1]};
 `
 
 const TagLink = ({ children, ...props }) => {
   return (
-    <Div {...props}>
-      <Div>{children}</Div>
-    </Div>
+    <StyledLink to={`/iniciativas?q=${children}`} kind="grayscale" light {...props}>
+      <Square />
+      <span>{children}</span>
+    </StyledLink>
   )
 }
 
 TagLink.propTypes = {
-  children: PropTypes.any
+  children: PropTypes.string.isRequired
 }
 
 export default TagLink
--- src/components/molecules/TagLink/index.test.js
+++ src/components/molecules/TagLink/index.test.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import TagLink from '.'
 
-const wrap = (props = {}) => shallow(<TagLink {...props} />).dive()
+const wrap = (props = {}) => shallow(<TagLink {...props}>test</TagLink>).dive()
 
 it('renders children when passed in', () => {
-  const wrapper = wrap({ children: 'test' })
+  const wrapper = wrap()
   expect(wrapper.contains('test')).toBe(true)
 })
 
