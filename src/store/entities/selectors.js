be99e11e8d6ddbc5efcee5e5292a7d47799aad9a
Diego Haz <hazdiego@gmail.com>
2016-12-13 05:26:27 -0200

    Add photo state to the entities store

--- src/store/entities/selectors.js
+++ src/store/entities/selectors.js
@@ -3,31 +3,36 @@ import { arrayOf } from 'normalizr'
 import initiative from '../initiative/schema'
 import user from '../user/schema'
 import tag from '../tag/schema'
+import photo from '../photo/schema'
 
 export const initialState = {
   initiatives: {},
-  users: {},
-  tags: {}
+  photos: {},
+  tags: {},
+  users: {}
 }
 
 export const getNormalizedInitiatives = (state = initialState) => state.initiatives || {}
-export const getNormalizedUsers = (state = initialState) => state.users || {}
+export const getNormalizedPhotos = (state = initialState) => state.photos || {}
 export const getNormalizedTags = (state = initialState) => state.tags || {}
+export const getNormalizedUsers = (state = initialState) => state.users || {}
 
 export const getInitiatives = (state = initialState, ids) =>
   denormalize(ids || Object.keys(getNormalizedInitiatives(state)), state, arrayOf(initiative))
-
 export const getInitiative = (state = initialState, id) =>
   denormalize(getNormalizedInitiatives(state)[id], state, initiative)
 
-export const getUsers = (state = initialState, ids) =>
-  denormalize(ids || Object.keys(getNormalizedUsers(state)), state, arrayOf(user))
-
-export const getUser = (state = initialState, id) =>
-  denormalize(getNormalizedUsers(state)[id], state, user)
+export const getPhotos = (state = initialState, ids) =>
+  denormalize(ids || Object.keys(getNormalizedPhotos(state)), state, arrayOf(photo))
+export const getPhoto = (state = initialState, id) =>
+  denormalize(getNormalizedPhotos(state)[id], state, photo)
 
 export const getTags = (state = initialState, ids) =>
   denormalize(ids || Object.keys(getNormalizedTags(state)), state, arrayOf(tag))
-
 export const getTag = (state = initialState, id) =>
   denormalize(getNormalizedTags(state)[id], state, tag)
+
+export const getUsers = (state = initialState, ids) =>
+  denormalize(ids || Object.keys(getNormalizedUsers(state)), state, arrayOf(user))
+export const getUser = (state = initialState, id) =>
+  denormalize(getNormalizedUsers(state)[id], state, user)
