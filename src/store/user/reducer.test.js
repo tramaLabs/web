8f250d2dde392f9687e504a2815611f0c8f3e27b
Diego Haz <hazdiego@gmail.com>
2016-12-13 21:46:30 -0200

    Update reducer tests

--- src/store/user/reducer.test.js
+++ src/store/user/reducer.test.js
@@ -3,25 +3,23 @@ import * as actions from './actions'
 import { AUTH_LOGOUT } from '../auth/actions'
 import reducer from './reducer'
 
+const altState = {
+  ...initialState,
+  currentId: 5
+}
+
 it('returns the initial state', () => {
   expect(reducer(undefined, {})).toEqual(initialState)
 })
 
 it('handles CURRENT_USER_READ_SUCCESS', () => {
-  expect(reducer(initialState, {
-    type: actions.CURRENT_USER_READ_SUCCESS,
-    result: 1
-  })).toEqual({
-    ...initialState,
-    currentId: 1
-  })
+  const action = { type: actions.CURRENT_USER_READ_SUCCESS, result: 1 }
+  expect(reducer(initialState, action)).toEqual({ ...initialState, currentId: 1 })
+  expect(reducer(altState, action)).toEqual({ ...altState, currentId: 1 })
 })
 
 it('handles AUTH_LOGOUT', () => {
-  expect(reducer({
-    ...initialState,
-    currentId: 1
-  }, {
-    type: AUTH_LOGOUT
-  })).toEqual(initialState)
+  const action = { type: AUTH_LOGOUT }
+  expect(reducer(initialState, action)).toEqual(initialState)
+  expect(reducer(altState, action)).toEqual({ ...altState, currentId: initialState.currentId })
 })
