ac0459b89435a9db6936a88b546155633d7976da
Diego Haz <hazdiego@gmail.com>
2016-12-13 22:33:15 -0200

    Update Spinner component

--- src/components/atoms/Spinner/index.js
+++ src/components/atoms/Spinner/index.js
@@ -4,19 +4,11 @@ import styled, { keyframes } from 'styled-components'
 import { colors, reverseColors } from 'components/globals'
 
 const loading = keyframes`
-  0% { transform: rotate(0deg); }
+  0% { transform: rotate(90deg); }
   25% { transform: rotate(180deg); }
-  50% { transform: rotate(180deg); }
-  75% { transform: rotate(360deg); }
+  50% { transform: rotate(180deg) scale(1.5); }
+  75% { transform: rotate(270deg); }
   100% { transform: rotate(360deg); }
-`
-
-const innerLoading = keyframes`
-  0% { transform: scaleY(0); }
-  25% { transform: scaleY(0); }
-  50% { transform: scaleY(1); }
-  75% { transform: scaleY(1); }
-  100% { transform: scaleY(0); }
 `
 
 const Spinner = styled.div`
@@ -24,23 +16,10 @@ const Spinner = styled.div`
   margin: 0 auto;
   width: 1em;
   height: 1em;
-  animation: ${loading} 2.3s infinite ease;
-  border: 0.125em solid ${(props) =>
+  animation: ${loading} 1.5s infinite ease-in-out;
+  background-color: ${(props) =>
     props.light ? reverseColors[props.kind][1] : colors[props.kind][1]
   };
-
-  &:after {
-    content: '';
-    vertical-align: top;
-    display: block;
-    width: 100%;
-    height: 100%;
-    transform-origin: top;
-    animation: ${innerLoading} 2.3s infinite ease-in;
-    background-color: ${(props) =>
-      props.light ? reverseColors[props.kind][1] : colors[props.kind][1]
-    };
-  }
 `
 
 Spinner.propTypes = {
