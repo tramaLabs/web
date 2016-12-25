d1ccd523f0f61c9f09a9333d6402ddafc25d2960
Diego Haz <hazdiego@gmail.com>
2016-12-25 16:19:27 -0200

    Add utf-8 charset metatag

--- src/components/Html.js
+++ src/components/Html.js
@@ -8,6 +8,7 @@ const Html = ({ styles, assets, state, content }) => {
   return (
     <html lang="pt-br" {...attrs}>
       <head>
+        <meta charSet="utf-8" />
         {helmet.title.toComponent()}
         {helmet.meta.toComponent()}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
