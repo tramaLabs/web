bf0387b2ee236d9d189458e03f24547eba989a80
Diego Haz <hazdiego@gmail.com>
2017-02-05 01:16:19 -0200

    Update EditableLine component

--- src/components/molecules/EditableLine/index.js
+++ src/components/molecules/EditableLine/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { EditorState } from 'draft-js'
+import React, { PropTypes } from 'react'
+import { EditorState, ContentState } from 'draft-js'
 import Editor from 'draft-js-plugins-editor'
 import createSingleLinePlugin from 'draft-js-single-line-plugin'
 
@@ -7,14 +7,34 @@ const singleLinePlugin = createSingleLinePlugin()
 const plugins = [singleLinePlugin]
 
 class EditableLine extends React.Component {
+  static propTypes = {
+    content: PropTypes.string
+  }
+
   state = {
     editorState: EditorState.createEmpty()
+  }
+
+  componentDidMount() {
+    const { content } = this.props
+    if (content) {
+      this.onChange(EditorState.createWithContent(ContentState.createFromText(content)))
+    }
   }
 
   onChange = (editorState) => {
     this.setState({
       editorState
     })
+  }
+
+  getContent() {
+    const { editorState } = this.state
+    return editorState.getCurrentContent().getPlainText()
+  }
+
+  focus() {
+    this.editor.focus()
   }
 
   render() {
@@ -24,6 +44,7 @@ class EditableLine extends React.Component {
         onChange={this.onChange}
         plugins={plugins}
         blockRenderMap={singleLinePlugin.blockRenderMap}
+        ref={editor => { this.editor = editor }}
         {...this.props}
       />
     )
