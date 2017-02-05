995bc0427f2c30d7cc92eee39744caf12b5f4797
gabrielarrais <gabriel.arrais@vtex.com.br>
2017-02-05 02:29:07 -0200

    adds initiative mock in Forum component test

--- src/components/organisms/Forum/index.test.js
+++ src/components/organisms/Forum/index.test.js
@@ -2,7 +2,13 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import Forum from '.'
 
-const wrap = (props = {}) => shallow(<Forum initiatives={[{ id: 1 }]} {...props} />)
+const initiative = {
+  title: 'test initiative',
+  tags: [],
+  photo: {}
+}
+
+const wrap = (props = {}) => shallow(<Forum initiative={initiative} {...props} />)
 
 it('renders props when passed in', () => {
   const wrapper = wrap({ id: 'foo' })
