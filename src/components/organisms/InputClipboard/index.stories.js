e45e06c9800ba6766403dd8571f4db9c64649a4c
Leonardo Kaplan <contato.leonardokaplan@gmail.com>
2017-02-05 19:39:08 -0200

    tests "fix"

--- src/components/organisms/InputClipboard/index.stories.js
+++ src/components/organisms/InputClipboard/index.stories.js
@@ -1,32 +1,8 @@
 import React from 'react'
 import { storiesOf } from '@kadira/storybook'
-import { IconButton } from 'components'
+import { InputClipboard } from 'components'
 
-storiesOf('IconButton', module)
+storiesOf('InputClipboard', module)
   .add('default', () => (
-    <IconButton icon="close">Hello</IconButton>
-  ))
-  .add('transparent', () => (
-    <IconButton icon="close" transparent>Hello</IconButton>
-  ))
-  .add('with icon on right', () => (
-    <IconButton icon="close" right>Hello</IconButton>
-  ))
-  .add('responsive', () => (
-    <IconButton icon="close" responsive>Decrease panel width</IconButton>
-  ))
-  .add('responsive with breakpoint', () => (
-    <IconButton icon="close" breakpoint={300} responsive>Decrease panel width to 300</IconButton>
-  ))
-  .add('without text', () => (
-    <IconButton icon="close" />
-  ))
-  .add('collapsed', () => (
-    <IconButton icon="close" collapsed>Hello</IconButton>
-  ))
-  .add('height', () => (
-    <IconButton icon="close" height={100}>Hello</IconButton>
-  ))
-  .add('collapsed with height', () => (
-    <IconButton icon="close" height={30} collapsed>Hello</IconButton>
+    <InputClipboard text="http://www.link.com" />
   ))
--- src/components/organisms/InputClipboard/index.test.js
+++ src/components/organisms/InputClipboard/index.test.js
@@ -1,41 +1,12 @@
 import React from 'react'
-import { mount, shallow } from 'enzyme'
-import IconButton from '.'
+import { shallow } from 'enzyme'
+import InputClipboard from '.'
 
-const wrap = (props = {}) => shallow(<IconButton icon="close" {...props} />)
+const wrap = (props = {}) => shallow(<InputClipboard text="http://www.link.com" {...props} />)
 
-it('mounts with different combination of props', () => {
-  mount(<IconButton icon="close">test</IconButton>)
-  mount(<IconButton icon="close" right>test</IconButton>)
-  mount(<IconButton icon="close" responsive>test</IconButton>)
-  mount(<IconButton icon="close" collapsed>test</IconButton>)
-  mount(<IconButton icon="close" right responsive>test</IconButton>)
-  mount(<IconButton icon="close" />)
-  mount(<IconButton icon="close" right />)
-  mount(<IconButton icon="close" responsive />)
-  mount(<IconButton icon="close" right responsive />)
-})
-
-it('renders children when passed in', () => {
-  const wrapper = wrap({ children: 'test' })
-  expect(wrapper.contains('test')).toBe(true)
-})
 
 it('renders props when passed in', () => {
   const wrapper = wrap({ id: 'foo' })
   expect(wrapper.find({ id: 'foo' })).toHaveLength(1)
 })
 
-it('renders icon on left by default', () => {
-  const wrapper = wrap({ children: 'test' })
-  const icon = wrapper.find({ icon: 'close' })
-  const parent = icon.parent()
-  expect(parent.children().first().prop('icon')).toBe('close')
-})
-
-it('renders icon on right when prop is passed in', () => {
-  const wrapper = wrap({ children: 'test', right: true })
-  const icon = wrapper.find({ icon: 'close' })
-  const parent = icon.parent()
-  expect(parent.children().last().prop('icon')).toBe('close')
-})
