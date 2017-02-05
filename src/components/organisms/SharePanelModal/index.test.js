34a1b6664e435ac613fe3bcd49786223a80c6ce7
Leonardo Kaplan <contato.leonardokaplan@gmail.com>
2017-02-05 02:23:55 -0200

    sharepanelmodal test

--- src/components/organisms/SharePanelModal/index.test.js
+++ src/components/organisms/SharePanelModal/index.test.js
@@ -1,29 +1,13 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import InitiativeUserListModal from '.'
+import SharePanelModal from '.'
 
-const initiative = {
-  users: [
-    { id: 0, name: 'Jeane', picture: 'jeane.jpg', services: { facebook: 'jeane' } },
-    { id: 1, name: 'Diego', picture: 'diego.jpg', services: { facebook: 'diego' } }
-  ]
-}
+const initiative = {}
 
-const wrap = (props = {}) => shallow(<InitiativeUserListModal initiative={initiative} {...props} />)
+const wrap = (props = {}) => shallow(<SharePanelModal initiative={initiative} {...props} />)
 
 it('renders props when passed in', () => {
   const wrapper = wrap({ id: 'foo' })
   expect(wrapper.find({ id: 'foo' })).toHaveLength(1)
 })
 
-it('renders user names', () => {
-  const wrapper = wrap()
-  expect(wrapper.contains('Jeane')).toBe(true)
-  expect(wrapper.contains('Diego')).toBe(true)
-})
-
-it('renders user pictures', () => {
-  const wrapper = wrap()
-  expect(wrapper.find({ src: 'jeane.jpg' })).toHaveLength(1)
-  expect(wrapper.find({ src: 'diego.jpg' })).toHaveLength(1)
-})
