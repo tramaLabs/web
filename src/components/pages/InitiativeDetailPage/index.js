836c6db9c660e1d1be5bbcd59d718f1d42be174c
Leonardo Kaplan <contato.leonardokaplan@gmail.com>
2017-02-23 15:14:22 -0300

    metatags on initiative page

--- src/components/pages/InitiativeDetailPage/index.js
+++ src/components/pages/InitiativeDetailPage/index.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react'
 
 import { PageTemplate, Header, InitiativeDetailHero } from 'components'
+import Helmet from 'react-helmet'
 
 const InitiativeDetailPage = ({ initiative }) => {
   return (
@@ -9,6 +10,15 @@ const InitiativeDetailPage = ({ initiative }) => {
       hero={initiative && <InitiativeDetailHero initiative={initiative} />}
       footer={<div />}
     >
+      <Helmet
+        title="Trama"
+        meta={[
+            { name: 'description', content: initiative.description },
+            { property: 'og:site_name', content: 'Trama' },
+            { property: 'og:image', content: initiative.picture },
+            { property: 'og:image:type', content: 'image/png' },
+        ]}
+      />
       {initiative && initiative.description}
     </PageTemplate>
   )
