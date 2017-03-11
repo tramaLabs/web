58def55b04248e79519dc2508fdea12848212e3b
gabrielarrais <gabriel.arrais@vtex.com.br>
2017-03-11 18:32:40 -0300

    fixes offerform redux form name

--- src/containers/OfferForm.js
+++ src/containers/OfferForm.js
@@ -15,9 +15,8 @@ const onSubmit = (data, dispatch) => new Promise((resolve, reject) => {
   if (error.status === 401) {
     throw new SubmissionError({ _error: 'Você precisa estar conectado para oferecer' })
   }
-  console.log(error)
   throw new SubmissionError({
-    _error: 'O servidor está instável. Por favor, tente novamente mais tarde.'
+    _error: 'Ocorreu um erro inesperado. Por favor, tente novamente mais tarde.'
   })
 })
 
@@ -25,15 +24,17 @@ const validate = createValidator({
   quantity: [required, minLength(1), maxLength(90)],
 })
 
-const mapStateToProps = (state, ownProps) => ({
-  connected: !!fromUser.getCurrentDetail(state),
-  user: fromEntities.getDetail(state, 'user', fromUser.getCurrentDetail(state)),
-  initialValues: {
+const mapStateToProps = (state, ownProps) => {
+  return {
+    connected: !!fromUser.getCurrentDetail(state),
     user: fromEntities.getDetail(state, 'user', fromUser.getCurrentDetail(state)),
-    demand: ownProps.demand,
-    initiative: ownProps.initiative,
+    initialValues: {
+      user: fromEntities.getDetail(state, 'user', fromUser.getCurrentDetail(state)),
+      demand: ownProps.demand,
+      initiative: ownProps.initiative,
+    }
   }
-})
+}
 
 OfferFormContainer.propTypes = {
   initiative: PropTypes.shape({
@@ -45,7 +46,6 @@ OfferFormContainer.propTypes = {
 }
 
 export default connect(mapStateToProps)(reduxForm({
-  form: 'OfferFormContainer',
   onSubmit,
   validate,
 })(OfferFormContainer))
