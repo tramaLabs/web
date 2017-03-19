56e5ddb6f7323842ab8352be3c510fe8dbe1a079
gabrielarrais <gabriel.arrais@vtex.com.br>
2017-03-19 18:13:53 -0300

    adds a provisory text to initiatives creation page

--- src/components/pages/InitiativeCreationPage/index.js
+++ src/components/pages/InitiativeCreationPage/index.js
@@ -11,7 +11,7 @@ const InitiativeCreationPage = () => {
         <div>
           <Heading palette="primary">Vamos abrir a sua iniciativa</Heading>
           <Paragraph>
-            Cae dolecum fuga. Et ut volupta tusdae volupta quiandae. Et accusci pienis doluptatis solorum fugit eos am fugiaep ercimusam ide volorios rem. Ihit molorib ernat. Raepedi dolupit veliciam corion et voluptat ligenient eliciis aligent. Usdae del in nos que corecus, sequo venihitatur am rati tem vid ulparuntor reria sollessed quos dem ad quia ad quassunturit ommolore mo maxim cus, sinven.
+            Crie aqui sua iniciativa. Explique seu propósito, como participar? Uma boa descrição é chave para o sucesso da sua iniciativa'
           </Paragraph>
         </div>
       }
