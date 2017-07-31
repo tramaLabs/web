8bb0029a1a920d18d9f1de75fbdc2a95a2ab4f61
onathanmiranda <nathanhp@hotmail.com>
2017-07-31 01:19:11 -0300

    Change logos to BETA version. And added title attribute to logoImage component.

--- src/components/atoms/LogoImage/index.js
+++ src/components/atoms/LogoImage/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 import logo from './logo.png'
 
-const LogoImage = (props) => <img alt="Logo" {...props} src={logo} />
+const LogoImage = (props) => <img title="Logo Trama BETA" alt="Logo Trama BETA" {...props} src={logo} />
 
 export default LogoImage
