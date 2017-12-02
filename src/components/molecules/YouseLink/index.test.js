316bca36afc8bbc0d93a557880093c6ae5c211fd
Darlene <darlenedms3@gmail.com>
2017-12-02 17:35:05 -0200

    Change tests that didn't make sense, it was copied from somewhere else

--- src/components/molecules/YouseLink/index.test.js
+++ src/components/molecules/YouseLink/index.test.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import LogoLink from '.'
+import YouseLink from '.'
 
-const wrap = (props = {}) => shallow(<LogoLink {...props} />)
+const wrap = (props = {}) => shallow(<YouseLink {...props} />)
 
 it('renders props when passed in', () => {
-  const wrapper = wrap({ id: 'foo' })
-  expect(wrapper.find({ id: 'foo' })).toHaveLength(1)
+  const wrapper = wrap({ width: 100, height: 50 })
+  expect(wrapper.find({ width: 100 })).toHaveLength(1)
+  expect(wrapper.find({ height: 50 })).toHaveLength(1)
 })
--- src/components/organisms/InitiativeDetailInfoModal/index.test.js
+++ src/components/organisms/InitiativeDetailInfoModal/index.test.js
@@ -1,27 +1,18 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import UserListModal from '.'
+import InitiativeDetailInfoModal from '.'
 
-const users = [
-    { id: 0, name: 'Jeane', picture: 'jeane.jpg', services: { facebook: 'jeane' } },
-    { id: 1, name: 'Diego', picture: 'diego.jpg', services: { facebook: 'diego' } }
-]
-
-const wrap = (props = {}) => shallow(<UserListModal users={users} {...props} />)
+const wrap = (props = {}) => shallow(<InitiativeDetailInfoModal {...props} />)
 
 it('renders props when passed in', () => {
-  const wrapper = wrap({ id: 'foo' })
-  expect(wrapper.find({ id: 'foo' })).toHaveLength(1)
-})
-
-it('renders user names', () => {
-  const wrapper = wrap()
-  expect(wrapper.contains('Jeane')).toBe(true)
-  expect(wrapper.contains('Diego')).toBe(true)
-})
-
-it('renders user pictures', () => {
-  const wrapper = wrap()
-  expect(wrapper.find({ src: 'jeane.jpg' })).toHaveLength(1)
-  expect(wrapper.find({ src: 'diego.jpg' })).toHaveLength(1)
+  const props = {
+    title: 'My Modal',
+    initiative: {
+      id: 123,
+      title: 'Super Initiative'
+    },
+    handleSubmit: () => {}
+  }
+  const wrapper = wrap(props)
+  expect(wrapper.find({ title: 'My Modal' })).toHaveLength(1)
 })
